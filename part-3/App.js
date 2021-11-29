f7900ecbc8cb205e90c53831f1d221da3250eb54
Brian-15 <bcirieco@gmail.com>
2021-11-29 15:59:23 -0800

    add part 43

---
+++ part-3/App.js
@@ -0,0 +1,9 @@
+const App = (props) => {
+    return (
+        <div>
+            <Person name="Marzia" age={25} hobbies={["gaming", "ski", "cinema"]} />
+            <Person name="Paolo" age={18} hobbies={["ice hockey", "drag", "chess"]} />
+            <Person name="Paola" age={17} hobbies={["twitch", "surfing", "cinema"]} />
+        </div>
+    )
+};
